baea96cb6d8ad8a4efe51b38ce37e60f3d83ad23
Mohammed Abdul Sofiyan <sofiyan@Mohammeds-MacBook-Pro.local>
2023-06-05 20:37:42 +0530

    Initialized the Bitespeed's  Identity Reconciliation service

---
+++ src/test/java/com/bitespeed/BitespeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitespeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitespeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
